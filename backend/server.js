38687b7d05164b2e00077cf4a02d527ed0c2a551
tanush27 <tanush.jadhav.btech2021@sitpune.edu.in>
2024-03-29 15:59:46 +0530

    Added till Signup Endpoint

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // Middleware for parsing incoming requests with JSON payload (from  req.body)
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   //root path/route
+//   res.send("Hello World!");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server running on port ${PORT}`);
+});
