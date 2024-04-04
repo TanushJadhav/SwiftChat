ce2061bde7bd9759be43d208dff755976ee76e81
tanush27 <tanush.jadhav.btech2021@sitpune.edu.in>
2024-04-04 21:10:42 +0530

    created AuthContext, Completed Login and Signup Functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
+import { Navigate, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import { useAuthContext } from './context/AuthContext.jsx';
+
 import './App.css'
 import Home from './pages/home/Home.jsx'
-// import Login from './pages/login/Login.jsx'
-// import Signup from './pages/signup/Signup.jsx'
+import Login from './pages/login/Login.jsx'
+import Signup from './pages/signup/Signup.jsx'
+
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <Home />
+      <Routes>
+        <Route path="/" element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login />} />
+        <Route path="/signup" element={authUser ? <Navigate to="/" /> : <Signup />} />
+      </Routes>
+      <Toaster/>
     </div>
   )
 }
