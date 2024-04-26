ae69830fb66937bc5f3e2b19634e7deb412d66fa
tanush27 <tanush.jadhav.btech2021@sitpune.edu.in>
2024-04-26 11:50:55 +0530

    Updated Socket.io URL for  real time deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const  SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://swiftchat-bt8s.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
